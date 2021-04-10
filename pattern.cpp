a7e08206e4a2bb3a71e4ce48fb688e4d1ffaca26
TangYan <tangyan1412@foxmail.com>
2021-04-10 19:21:36 +0800

    arg modify

--- pattern.cpp
+++ pattern.cpp
@@ -8,7 +8,7 @@ static int directY[] = { 1, 1, 0, -1 };
 int patternLib[PATTERN_SIZE] = { PATTERN_NULL };
 int patternScore[PATTERN_SIZE] = { PATTERN_NULL };
 
-static int baseScore[10] = { 0, 100000, 10000, 51 ,58, 22 ,21, 8 ,10, 8 };
+static int baseScore[10] = { 0, 100000, 10000, 44 ,51, 20, 26, 8 ,13, 2 };
 static int sigma = 2;
 
 static bool inited = false;
