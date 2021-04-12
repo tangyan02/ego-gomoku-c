2d571cd931456d9fb46a1745b0ed5b436cfd3873
Tang Yan <tangyan1412@foxmail.com>
2021-04-12 18:57:33 +0800

    neighbor modify

--- levelProcessor.cpp
+++ levelProcessor.cpp
@@ -1,53 +1,112 @@
-#include "stdafx.h"
 #include "levelProcessor.h"
-#include "gameMap.h"
-#include "patternRecorder.h"
 #include "PointsFactory.h"
 #include "analyzer.h"
+#include "gameMap.h"
+#include "patternRecorder.h"
+#include "stdafx.h"
+#include <map>
+#ifdef __GNUC__
+#include <ext/hash_map>
+using namespace __gnu_cxx;
+#else
+#include <hash_map>
+#endif
 
+map<int, hash_map<int, point> > neighborSortTree[3];
+
+int singleScore[3][20][20];
 
 extern int boardSize;
 
 static int scoreList[512];
 
-static int directX[] = { 0, 1, 1, 1 };
+static int directX[] = {0, 1, 1, 1};
 
-static int directY[] = { 1, 1, 0, -1 };
+static int directY[] = {1, 1, 0, -1};
 
-extern int blackPattern[20][20][4];
-extern int whitePattern[20][20][4];
+extern int neighborCount[20][20];
 
-extern int blackPatternCountInNull[10];
-extern int whitePatternCountInNull[10];
-
-extern int score[10];
+extern int blackLineKey[20][20][4];
+extern int whiteLineKey[20][20][4];
+extern int patternScore[PATTERN_SIZE];
 
 extern points moveHistory;
 
-extern Color ** map;
-
-static const int crossWeight = 50;
-static const int crossLimit = 3;
-
-int getScore(int x, int y, Color color) {
+static int getScore(int x, int y, Color color)
+{
 	int value = 0;
-	for (int k = 0; k < 4; k++) {
+	for (int k = 0; k < 4; k++)
+	{
 		if (color == BLACK)
-			value += score[blackPattern[x][y][k]];
+		{
+			value += patternScore[blackLineKey[x][y][k]];
+			value += patternScore[whiteLineKey[x][y][k]] / 2;
+		}
 		if (color == WHITE)
-			value += score[whitePattern[x][y][k]];
+		{
+			value += patternScore[whiteLineKey[x][y][k]];
+			value += patternScore[blackLineKey[x][y][k]] / 2;
+		}
 	}
 	return value;
 }
 
-void qsort(point *list, int score[], int l, int r) {
+void updateNeighborSortTree(int x, int y, bool isAdd)
+{
+	if (neighborCount[x][y] > 1)
+	{
+		return;
+	}
+	for (Color color = 1; color <= 2; color++)
+	{
+		if (singleScore[color][x][y] >= 0 )
+		{
+			int score = singleScore[color][x][y];
+			int pointKey = x * boardSize + y;
+
+			map<int, hash_map<int, point> >::iterator item = neighborSortTree[color].find(score);
+			if (item != neighborSortTree[color].end())
+			{
+				item->second.erase(pointKey);
+			}
+
+			singleScore[color][x][y] = -1;
+		}
+		if (neighborCount[x][y] > 0)
+		{
+			int score = getScore(x, y, color);
+			int pointKey = x * boardSize + y;
+
+			map<int, hash_map<int, point> >::iterator item = neighborSortTree[color].find(score);
+			if (item == neighborSortTree[color].end())
+			{
+				hash_map<int, point> hashMap;
+				hashMap.insert(pair<int, point>(pointKey, point(x, y)));
+				neighborSortTree[color].insert(pair<int, hash_map<int, point> >(score, hashMap));
+			}
+			else
+			{
+				item->second.insert(pair<int, point>(pointKey, point(x, y)));
+			}
+
+			singleScore[color][x][y] = score;
+		}
+	}
+}
+
+void qsort(point *list, int score[], int l, int r)
+{
 	int x = l;
 	int y = r;
 	int mid = score[(x + y) / 2];
-	while (x < y) {
-		while (score[x] > mid) x++;
-		while (score[y] < mid) y--;
-		if (x <= y) {
+	while (x < y)
+	{
+		while (score[x] > mid)
+			x++;
+		while (score[y] < mid)
+			y--;
+		if (x <= y)
+		{
 			int t = score[x];
 			score[x] = score[y];
 			score[y] = t;
@@ -58,61 +117,65 @@ void qsort(point *list, int score[], int l, int r) {
 			y--;
 		}
 	}
-	if (x < r) qsort(list, score, x, r);
-	if (l < y) qsort(list, score, l, y);
+	if (x < r)
+		qsort(list, score, x, r);
+	if (l < y)
+		qsort(list, score, l, y);
 }
 
-bool cross(int x1, int y1, int x2, int y2) {
-	if (x1 == x2 && abs(y1 - y2) <= crossLimit) {
-		return true;
+void selectAndSortPoints(points *neighbors, Color color)
+{
+	int count = 0;
+	map<int, hash_map<int, point> >::iterator iter;
+	for(iter = neighborSortTree[color].begin(); iter!=neighborSortTree[color].end(); iter++){
+		count += iter->second.size();
+		hash_map<int, point>::iterator item = iter->second.begin();
+		for (; item != iter->second.end();item++){
+			for (int x = 0; x < 20;x++)
+				for (int y = 0; y < 20;y++){
+					if(item->second.x == x && item->second.y == y){
+						printf("%d,%d  ", x, y);
+					}
+				}
+		}
 	}
-	if (y1 == y2 && abs(x1 - x2) <= crossLimit) {
-		return true;
+	printf("\n");
+	for (int i = 0; i < neighbors->count;i++)
+	{
+		for (int x = 0; x < 20; x++)
+			for (int y = 0; y < 20; y++)
+			{
+				if (neighbors->list[i].x == x && neighbors->list[i].y == y)
+				{
+					printf("%d,%d  ", x, y);
+				}
+			}
 	}
-	if (x1 - x2 == y1 - y2 && abs(x1 - x2) <= crossLimit) {
-		return true;
+	if (count != neighbors->count)
+	{
+		printf("\nnum not same!!! color=%d count=%d neighborsCount=%d\n",color , count, neighbors->count);
 	}
-	if (x1 + x2 == y1 + y2 && abs(y1 - y2) <= crossLimit) {
-		return true;
+	else
+	{
+		printf("num same!!!\n");
 	}
-	return false;
-}
 
-void selectAndSortPoints(points *neighbors, Color color) {
-	//连长五
+	//锟斤拷锟斤拷锟斤拷
 	if (tryFiveAttack(color, neighbors))
 		return;
-	//堵冲四
+	//锟铰筹拷锟斤拷
 	if (tryFourDefence(color, neighbors))
 		return;
-	//对付活三
+	//锟皆革拷锟斤拷锟斤拷
 	if (tryThreeDefence(color, neighbors))
 		return;
 
-	//排序
-	point* list = neighbors->list;
-	
-	//历史2步的米字位置优先计算
-	/*point pointLast1;
-	point pointLast2;
-	if (moveHistory.count > 2) {
-		pointLast1 = moveHistory.list[moveHistory.count - 1];
-		pointLast2 = moveHistory.list[moveHistory.count - 2];
-	}*/
+	//锟斤拷锟斤拷
+	point *list = neighbors->list;
 
-	for (int i = 0; i < neighbors->count; i++) {
+	for (int i = 0; i < neighbors->count; i++)
+	{
 		scoreList[i] = getScore(list[i].x, list[i].y, color);
-
-		//如果和历史节点是米字位置关系，则优先计算
-		/*if (moveHistory.count > 2) {
-			if (cross(pointLast1.x, pointLast1.y, list[i].x, list[i].y)) {
-				scoreList[i] += crossWeight;
-			}
-			else if (cross(pointLast2.x, pointLast2.y, list[i].x, list[i].y)) {
-				scoreList[i] += crossWeight;
-			}
-		}*/
 	}
 	qsort(list, scoreList, 0, neighbors->count - 1);
 }
-
