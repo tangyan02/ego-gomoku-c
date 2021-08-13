b118448d403d315c0889dc46b00a292138412131
Tang Yan <tangyan1412@foxmail.com>
2021-08-13 22:30:28 +0800

    linux模式支持

--- main.cpp
+++ main.cpp
@@ -1,15 +1,15 @@
-#include "stdafx.h"
+#include "comboProcessor.h"
 #include "console.h"
+#include "game.h"
 #include "gameMap.h"
 #include "io.h"
-#include "game.h"
-#include "score.h"
+#include "learn.h"
 #include "levelProcessor.h"
-#include "pisqpipe.h"
 #include "pattern.h"
 #include "patternRecorder.h"
-#include "comboProcessor.h"
-#include "learn.h"
+#include "pisqpipe.h"
+#include "score.h"
+#include "stdafx.h"
 
 extern bool debugEnable;
 
@@ -23,55 +23,73 @@ extern int comboTimeOut;
 
 extern int timeOut;
 
-void testPlay() {
-	boardSize = 20;
-	debugEnable = true;
-	Color ** map = readMap();
-	gameResult result = search(WHITE, map);
-	printMapWithStar(map, result.result);
+void testPlay()
+{
+    boardSize = 20;
+    debugEnable = true;
+    Color **map = readMap();
+    gameResult result = search(WHITE, map);
+    printMapWithStar(map, result.result);
 }
 
-void removeStar(Color** map) {
-	for (int i = 0; i < boardSize; i++)
-	{
-		for (int j = 0; j < boardSize; j++)
-		{
-			if (map[i][j] == STAR) {
-				map[i][j] = NULL;
-			}
-		}
-	}
+void removeStar(Color **map)
+{
+    for (int i = 0; i < boardSize; i++)
+    {
+        for (int j = 0; j < boardSize; j++)
+        {
+            if (map[i][j] == STAR)
+            {
+                map[i][j] = NULL;
+            }
+        }
+    }
 }
 
-void bugTest() {
-	boardSize = 20;
-	debugEnable = true;
-	//searchLevel = 2;
-	Color ** map = readMap("input007.txt");
-	gameResult result = search(WHITE, map);
-	printMapWithStar(map, result.result);
+void bugTest()
+{
+    boardSize = 20;
+    debugEnable = true;
+    //searchLevel = 2;
+    Color **map = readMap("input007.txt");
+    gameResult result = search(WHITE, map);
+    printMapWithStar(map, result.result);
 }
 
-void caseTest() {
-	piskvorkMessageEnable = true;
-	boardSize = 20;
-	debugEnable = true;
-	comboTimeOut = 3000;
-	timeOut = 5000;
-	Color **map = readMap("input007.txt");
-	gameResult result = search(WHITE, map);
-	printMapWithStar(map, result.result);
+void caseTest()
+{
+    piskvorkMessageEnable = true;
+    boardSize = 20;
+    debugEnable = true;
+    comboTimeOut = 3000;
+    timeOut = 5000;
+    Color **map = readMap("input007.txt");
+    gameResult result = search(WHITE, map);
+    printMapWithStar(map, result.result);
+}
+
+void oneTurnSearch()
+{
+    piskvorkMessageEnable = true;
+    boardSize = 15;
+    debugEnable = true;
+    comboTimeOut = 2000;
+    timeOut = 3000;
+    Color **map = readMap("input.txt");
+    gameResult result = search(WHITE, map);
+    printf("%d %d\n", result.result.x, result.result.y);
 }
 
 int main()
 {
-	//piskvork();
-	//testCombo();
-	//bugTest();
-	caseTest();
-	//testPlay();
-	//testPattern();
-	//testPatternRecorder();
-	//selfLearn();
-	return 0;
+    //piskvork();
+    //testCombo();
+    //bugTest();
+    // caseTest();
+    oneTurnSearch();
+    //testPlay();
+    //testPattern();
+    //testPatternRecorder();
+    //selfLearn();
+    return 0;
 }
